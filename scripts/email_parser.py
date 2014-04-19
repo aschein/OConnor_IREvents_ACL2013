b8069454b78b68c4e1e4ac57c32b30a2543c64d2
Aaron Schein <scheinaj@gmail.com>
2014-04-19 16:55:22 -0400

    added logging

--- scripts/email_parser.py
+++ scripts/email_parser.py
@@ -68,7 +68,8 @@ class EmailParser:
                     fr = lines[2].rstrip().split('From: ', 1)[1]
                     to = lines[3].rstrip().split('To: ', 1)[1].split(', ')
                 except IndexError:
-                    self.logger.error('Unexpected header lines:\n\t\t\t%s\n%s'%(full_path, ' '.join(lines[:6])))
+                    sep = "\n-----------------\n"
+                    self.logger.error('Unexpected header lines:\n\t\t\t%s\n%s%s%s'%(full_path, sep, ' '.join(lines[:6])), sep)
                     continue
                 self.messages[mi] = (ts, fr, to, body)
 
