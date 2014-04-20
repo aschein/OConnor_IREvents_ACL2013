716a22e537fc7a0b5609bc11020170ac55806a1a
Aaron Schein <scheinaj@gmail.com>
2014-04-19 21:55:43 -0400

    adding cleaning script

---
+++ scripts/cleaner.py
@@ -0,0 +1,27 @@
+if __name__ == '__main__':
+    entity_file = '/mnt/nfs/work1/wallach/aschein/data/enron/preproc/entities.tsv'
+    messages_file = '/mnt/nfs/work1/wallach/aschein/data/enron/preproc/messages.txt'
+
+    entities = []
+    with open(entity_file, 'r') as f:
+        lines = f.readlines()[1:]
+        for line in lines:
+            entities.append(lines.split('|')[0])
+
+    assert len(entities) == 300
+
+    with open(messages_file, 'r') as f:
+        lines = f.readlines()[1:]
+
+    hits = 0
+
+    for line in lines:
+        to = [x[1:-1] for x in line.split('|')[3][1:-1].split(',')]
+        if not sum([t in entities for t in to]):
+            continue
+        hits += 1
+
+    print '%f proportion hits'%(hits/float(len(lines)))
+
+
+
